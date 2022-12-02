cc7ef4c828bdc71d574dc1cd807077fc4018025e
Otto Heile <ottoeh@kth.se>
2022-12-02 18:59:57 +0100

    created a script for counting what number of students have which number of siblings

---
+++ count_siblings.sql
@@ -0,0 +1,20 @@
+SELECT number_of_siblings, COUNT(*) AS number_of_students
+FROM (
+    SELECT SUM(
+        CASE
+            WHEN id = student_id OR id = sibling_student_id
+            THEN 1
+            ELSE 0
+        END
+    ) AS number_of_siblings
+    FROM (
+        SELECT s.id, ss.student_id, ss.sibling_student_id
+        FROM student AS s
+        LEFT OUTER JOIN student_sibling AS ss
+        ON s.id = ss.student_id OR s.id = ss.sibling_student_id
+        ORDER BY s.id
+    ) AS foo
+    GROUP BY id
+) AS foo
+GROUP BY number_of_siblings
+HAVING COUNT(*) > 0;

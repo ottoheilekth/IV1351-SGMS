61fbe7dc0b4bbe4abf6231202b8a454e3ac7c34c
Otto Heile <ottoeh@kth.se>
2022-12-03 01:38:03 +0100

    created a script for finding instructors that have hosted/will host more lessons than a threshold

---
+++ instructor_lessons_over_threshold.sql
@@ -0,0 +1,12 @@
+SELECT COUNT(*) AS lessons_given, id AS instructor_id
+FROM (
+    SELECT i.id
+    FROM instructor AS i
+    INNER JOIN lesson AS l
+    ON i.id = l.instructor_id
+    WHERE EXTRACT(MONTH FROM l.date_and_time) = EXTRACT(MONTH FROM current_date)
+          AND EXTRACT(YEAR FROM l.date_and_time) = EXTRACT(YEAR FROM current_date)
+) AS foo
+GROUP BY id
+HAVING COUNT(*) > 0 /*Threshold*/
+ORDER BY lessons_given DESC;
